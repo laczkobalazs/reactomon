61cac473cf9dbd0083d526e29fc17c6430371b3d
laczkobalazs <laczko.balazs89@gmail.com>
2020-06-22 14:48:39 +0200

    test to push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PokemonList from "./components/PokemonList";
+import axios from "axios";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [initialUrl, setInitialUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(initialUrl).then((response) => {
+      setPokemonData(response.data.results.map((p) => p.name));
+    });
+  }, [initialUrl]);
+
+  const loadingPokemon = async (data) => {
+    let allPokemonData = await Promise.all(
+      data.map(async (pokemon) => {
+        let pokemonRecord = await axios.get(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(allPokemonData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="grid-container">
+          {pokemonData.map((pokemon, i) => {
+            return <Card key={i} pokemon={pokemon} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
